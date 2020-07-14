4f7d712401199b7a6057a510cab8ced5c3918529
idoharu <>
2020-07-14 09:47:12 +0900

    googleフォントのエラー解決。httpをhttpsと記載

--- head.php
+++ head.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?php echo $siteTitle; ?> | ウチのイッヌ</title>
   <link rel="stylesheet" type="text/css" href="common.css">
-  <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
   <!-- フォントアイコン -->
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
   <link rel="icon" href="img/innu-icon.png" type="image/png">
